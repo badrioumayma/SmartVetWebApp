6bc1e9e595f5a872af24f0f84703b6bf8dc7b802
oumayma <obadri@datahorizon.eu>
2024-11-26 22:07:25 +0100

    update

--- SmartVet_Front/src/app/app-routing.module.ts
+++ SmartVet_Front/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomePageComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- SmartVet_Front/src/app/app.module.ts
+++ SmartVet_Front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { FormComponent } from './form/form.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    FormComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
