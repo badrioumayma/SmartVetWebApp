1e5e8b4c96891169b41b26a91498baefe4205213
oumayma <obadri@datahorizon.eu>
2024-11-26 13:02:16 +0100

    update

--- SmartVet_Front/README.md
+++ SmartVet_Front/README.md
@@ -1,4 +1,4 @@
-# SmartVet
+# SmartVetFront
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.16.
 
--- SmartVet_Front/src/app/app.component.ts
+++ SmartVet_Front/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SmartVet';
+  title = 'SmartVet_Front';
 }
